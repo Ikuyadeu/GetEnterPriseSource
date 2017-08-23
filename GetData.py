02e0ce50b97cfd786677869ba0edd4c5bcac70fe
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2017-08-23 14:26:06 +0900

    Update GetData.py

--- GetData.py
+++ GetData.py
@@ -9,15 +9,10 @@ url = args[1]
 user = args[2]
 owner = args[3]
 project = args[4]
-# curl -i https://api.github.com/repos/Ikuyadeu/vscode-r/issues\?state=all\&sort=created > issues.json
-
-url = "https://api.github.com"
-owner = "Ikuyadeu"
-project = "vscode-r"
 
 url = url + "/repos/" + owner + "/" + project + "/"
 
-metricses = ["issues", "pulls", "issues/comments", "branches", "assignees"]
+metricses = ["issues", "pulls", "issues/comments", "branches", "assignees", "contributors", "languages"]
 
 for metrics in metricses:
-  os.system("curl -i "+ url + metrics + "\\?state=all\&sort=created\&per_page=100 > " + project + "-" + metrics.replace('/', '_') + ".json")
+  os.system("curl -u " + user + " "+ url + metrics + "\\?state=all\&sort=created\&per_page=100 > " + project + "-" + metrics.replace('/', '_') + ".json")
