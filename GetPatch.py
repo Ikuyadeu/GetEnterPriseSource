1c7ef76ddd2d46399a3db671dd1fe14bb405c9c3
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2017-08-25 17:12:29 +0900

    Create GetPatch.py

---
+++ GetPatch.py
@@ -0,0 +1,28 @@
+"""
+Usage:
+First use GetData.py and ready project-pulls.json
+python3 GetPatch.py pull_file outdir user password
+
+pullfile: github pull file from GetData.py (e.g. project-pulls.json)
+outdir: output directory
+user: your github id
+password: your github password
+"""
+
+import sys
+import json
+import os
+
+args = sys.argv
+pulls_file = args[1]
+outdir = args[2]
+user = args[3]
+password = args[4]
+
+with open(pulls_file, "r", encoding = "utf-8") as pulls:
+  pulls = [x for x in json.load(pulls) if x["closed_at"] is not None]
+
+patch_urls = [(x["base"]["repo"]["url"] + "/commits/" + x["merge_commit_sha"] , x["number"]) for x in pulls]
+print(patch_urls)
+for patch_url, number in patch_urls:
+  os.system("curl -u " + user + ":" + password " -H \"Accept: application/vnd.github.v3.patch\" " + patch_url + " -o " + outdir + "/" + str(number) + ".patch")
