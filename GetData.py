62fb7ace1a3e72bdacd79138768698b5754f62bd
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-09-12 19:20:01 +0900

    refactor

--- GetData.py
+++ GetData.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 # Usage: python3 GetData.py apiurl user password owner project
 import sys
-import os
-import requests
 import json
+import requests
 
 args = sys.argv
 
@@ -19,17 +18,18 @@ url = api + "/repos/" + owner + "/" + project + "/"
 metricses = ["issues", "pulls", "issues/comments","pulls/comments", "branches", "assignees", "contributors", "languages", "commits", "stats/contributors"]
 
 for metrics in metricses:
-  page=1
-  pages = []
-  header = api + "/repos/" + owner + "/" + project + "/"
-  while(True):
-    print(metrics + ":" + page)
-    url = header + metrics + "?state=all&sort=created&per_page=6"
-    resp = requests.get(url + '&page=' + str(page), auth=requests.auth.HTTPBasicAuth(user, password))
-    data = json.loads(resp.content.decode('utf-8'))
-    if len(data) <= 1:
-      break
-    page += 1
-    pages.extend(data)
-  with open(project + "-" + metrics.replace('/', '_') + "2.json", "w") as f:
-    json.dump(pages, f, indent=4)
+    page = 1
+    pages = []
+    header = api + "/repos/" + owner + "/" + project + "/"
+    while True:
+        print(metrics + ":" + page)
+        url = header + metrics + "?state=all&sort=created&per_page=6"
+        resp = requests.get(url + '&page=' + str(page),
+                            auth=requests.auth.HTTPBasicAuth(user, password))
+        data = json.loads(resp.content.decode('utf-8'))
+        if len(data) <= 1:
+            break
+        page += 1
+        pages.extend(data)
+    with open(project + "-" + metrics.replace('/', '_') + ".json", "w") as f:
+        json.dump(pages, f, indent=4)
