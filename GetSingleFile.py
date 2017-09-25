cf097ae49659d901d4bd65e82b76442edd48e2b5
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-09-25 15:54:14 +0900

    fix output

--- GetSingleFile.py
+++ GetSingleFile.py
@@ -29,36 +29,34 @@ URL = "https://api.github.com/repos/" \
 
 RAW_URL = "https://raw.githubusercontent.com/" + PROJECT_PATH
 
-PAGE = 1
-COMMITS = []
 
 AUTH = requests.auth.HTTPBasicAuth(USER, PASSWORD)
 PARAMS = {"path": PATH,
           "per_page": 100,
           "page": 1,}
+COMMITS = []
 
 # Get All commit log
 while True:
-    PARAMS["page"] = PAGE
     RESP = requests.get(URL,
                         params=PARAMS,
                         auth=AUTH)
     print(RESP.url)
-    DATA = json.loads(RESP.content.decode("utf-8"))
-    if len(DATA) <= 1:
+    CONTENT = json.loads(RESP.content.decode("utf-8"))
+    if len(CONTENT) <= 1:
         break
-    PAGE += 1
-    COMMITS.extend(DATA)
+    PARAMS["page"] += 1
+    COMMITS.extend(CONTENT)
 
 # Output Commit log
-with open(OUT_DIR + OWNER +"-" + PROJECT + ".json", "w") as f:
+OUT_FILE_PATH = OUT_DIR + OWNER +"-" + PROJECT + ".json"
+with open(OUT_FILE_PATH, "w") as f:
     json.dump(COMMITS, f, indent=4)
-    print("Output commit log to" + OUT_DIR + OWNER +"-" + PROJECT + ".json")
+    print("Output commit log to" + OUT_FILE_PATH)
 
 # Curl real files
-for commit in COMMITS:
+for i, commit in enumerate(reversed(COMMITS)):
     FILE_URL = RAW_URL + "/" + commit["sha"] + "/" + PATH
-    RESP = requests.get(FILE_URL, auth=AUTH)
-    CONTENT = RESP.content.decode("utf-8")
-    with open(OUT_DIR + commit["sha"] + "-" + PATH, "w") as f:
+    CONTENT = requests.get(FILE_URL, auth=AUTH).content.decode("utf-8")
+    with open(OUT_DIR + str(i) + "-" + PATH, "w") as f:
         f.write(CONTENT)
