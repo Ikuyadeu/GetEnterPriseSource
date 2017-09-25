9e970581ba5f9d82a199888ea7802c92d5e38468
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-09-25 14:56:39 +0900

    Refactor

--- GetReadme.py
+++ GetReadme.py
@@ -2,7 +2,8 @@
 """
 Usage: mkdir out
        pip3 install requests
-       python3 GetReadme.py YourGitHubID GitHubPassword owner project
+       python3 GetReadme.py YourGitHubID GitHubPassword owner project filePath
+       (e.g.) python3 python3 GetReadme.py GitTaro Gitpass Ikuyadeu vscode-r Readme.md
 """
 import json
 import sys
@@ -15,8 +16,8 @@ USER = ARGS[1]
 PASSWORD = ARGS[2]
 OWNER = ARGS[3]
 PROJECT = ARGS[4]
+PATH = ARGS[5]
 OUT_DIR = "out/"
-PATH = "README.md"
 
 URL = "https://api.github.com/repos/" \
       + OWNER \
@@ -48,11 +49,12 @@ while True:
     PAGE += 1
     PAGES.extend(DATA)
 
+with open(OUT_DIR + OWNER +"-" + PROJECT + ".json", "w") as f:
+    json.dump(PAGES, f, indent=4)
+    print("Output commit log to" + OUT_DIR + OWNER +"-" + PROJECT + ".json")
+
 for data in PAGES:
     FILE_URL = RAW_URL + "/" + data["sha"] + "/" + PATH
     RESP = requests.get(FILE_URL, auth=requests.auth.HTTPBasicAuth(USER, PASSWORD))
     with open(OUT_DIR + data["sha"] + "-" + PATH, "w") as f:
         f.write(RESP.content.decode("utf-8"))
-
-with open(OUT_DIR + OWNER +"-" + PROJECT + ".json", "w") as f:
-    json.dump(PAGES, f, indent=4)
