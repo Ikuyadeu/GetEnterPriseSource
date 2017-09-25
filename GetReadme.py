1dbd9351ad22d20291834b92b540fba99ff8727d
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-09-25 12:57:30 +0900

    Fix

--- GetReadme.py
+++ GetReadme.py
@@ -1,29 +1,50 @@
 # -*- coding: utf-8 -*-
-# Usage: pip3 install requests
-#        python3 GetReadme.py YourGitHubID GitHubPassword owner project
-import sys
-import os
+"""
+Usage: pip3 install requests
+       python3 GetReadme.py YourGitHubID GitHubPassword owner project
+"""
 import json
+import sys
+
 import requests
 
 ARGS = sys.argv
 
-user = ARGS[1]
-password = ARGS[2]
-owner = ARGS[3]
-project = ARGS[4]
+USER = ARGS[1]
+PASSWORD = ARGS[2]
+OWNER = ARGS[3]
+PROJECT = ARGS[4]
+PATH = "README.md"
 
-url = "https://api.github.com/repos/" + owner + "/" + project + "/commits?path=README.md&per_page=100"
+URL = "https://api.github.com/repos/" \
+      + OWNER \
+      + "/" \
+      + PROJECT \
+      + "/commits?path=" + PATH + "&per_page=100"
 
-page = 1
-pages = []
-while(True):
-    resp = requests.get(url + '&page=' + str(page), auth=requests.auth.HTTPBasicAuth(user, password))
-    data = json.loads(resp.content.decode('utf-8'))
-    if len(data) <= 1:
+RAW_URL = "https://raw.githubusercontent.com/" \
+          + OWNER \
+          + "/" \
+          + PROJECT \
+
+PAGE = 1
+PAGES = []
+
+while True:
+    RESP = requests.get(URL + '&page=' + str(PAGE),
+                        auth=requests.auth.HTTPBasicAuth(USER, PASSWORD))
+    DATA = json.loads(RESP.content.decode('utf-8'))
+    if len(DATA) <= 1:
         break
-    print(url + '&page=' + str(page))
-    page += 1
-    pages.extend(data)
-with open(owner +"-" + project + ".json", "w") as f:
-    json.dump(pages, f, indent=4)
+    print(URL + '&page=' + str(PAGE))
+    PAGE += 1
+    PAGES.extend(DATA)
+
+for data in PAGES:
+    FILE_URL = RAW_URL + "/" + data["sha"] + "/" + PATH
+    with open(data["sha"] + "-" + PATH, "w") as f:
+        f.write(requests.get(FILE_URL, auth=requests.auth.HTTPBasicAuth(USER, PASSWORD)))
+
+
+with open(OWNER +"-" + PROJECT + ".json", "w") as f:
+    json.dump(PAGES, f, indent=4)
