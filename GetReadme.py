bb74d6ebd46c8325318be689a70e22df983a024d
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-09-25 14:45:47 +0900

    Create Get README.md py

--- GetReadme.py
+++ GetReadme.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Usage: pip3 install requests
+Usage: mkdir out
+       pip3 install requests
        python3 GetReadme.py YourGitHubID GitHubPassword owner project
 """
 import json
@@ -14,26 +15,33 @@ USER = ARGS[1]
 PASSWORD = ARGS[2]
 OWNER = ARGS[3]
 PROJECT = ARGS[4]
+OUT_DIR = "out/"
 PATH = "README.md"
 
 URL = "https://api.github.com/repos/" \
       + OWNER \
       + "/" \
       + PROJECT \
-      + "/commits?path=" + PATH + "&per_page=100"
+      + "/commits"
 
 RAW_URL = "https://raw.githubusercontent.com/" \
           + OWNER \
           + "/" \
-          + PROJECT \
+          + PROJECT
 
 PAGE = 1
 PAGES = []
 
 while True:
-    RESP = requests.get(URL + '&page=' + str(PAGE),
+    PARAMS = (
+        ("path", PATH),
+        ("per_page", 100),
+        ("page", PAGE),
+    )
+    RESP = requests.get(URL,
+                        params=PARAMS,
                         auth=requests.auth.HTTPBasicAuth(USER, PASSWORD))
-    DATA = json.loads(RESP.content.decode('utf-8'))
+    DATA = json.loads(RESP.content.decode("utf-8"))
     if len(DATA) <= 1:
         break
     print(URL + '&page=' + str(PAGE))
@@ -42,9 +50,9 @@ while True:
 
 for data in PAGES:
     FILE_URL = RAW_URL + "/" + data["sha"] + "/" + PATH
-    with open(data["sha"] + "-" + PATH, "w") as f:
-        f.write(requests.get(FILE_URL, auth=requests.auth.HTTPBasicAuth(USER, PASSWORD)))
+    RESP = requests.get(FILE_URL, auth=requests.auth.HTTPBasicAuth(USER, PASSWORD))
+    with open(OUT_DIR + data["sha"] + "-" + PATH, "w") as f:
+        f.write(RESP.content.decode("utf-8"))
 
-
-with open(OWNER +"-" + PROJECT + ".json", "w") as f:
+with open(OUT_DIR + OWNER +"-" + PROJECT + ".json", "w") as f:
     json.dump(PAGES, f, indent=4)
