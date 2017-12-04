08b9b6147898a82c0d5794499d08f1f40aac2980
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-12-04 17:22:02 +0900

    fix getdata

--- GetData.py
+++ GetData.py
@@ -16,14 +16,16 @@ project = args[5]
 url = api + "/repos/" + owner + "/" + project + "/"
 
 metricses = ["issues", "pulls", "issues/comments","pulls/comments", "branches", "assignees", "contributors", "languages", "commits", "stats/contributors"]
+metricses = ["issues", "pulls", "issues/comments","pulls/comments", "branches", "assignees", "contributors", "languages", "commits", "stats/contributors"]
+
 
 for metrics in metricses:
     page = 1
     pages = []
     header = api + "/repos/" + owner + "/" + project + "/"
     while True:
-        print(metrics + ":" + page)
-        url = header + metrics + "?state=all&sort=created&per_page=6"
+        print(metrics + ":" + str(page))
+        url = header + metrics + "?state=all&sort=created&per_page=300"
         resp = requests.get(url + '&page=' + str(page),
                             auth=requests.auth.HTTPBasicAuth(user, password))
         data = json.loads(resp.content.decode('utf-8'))
